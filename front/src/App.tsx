cc034551dd05ba9c0a05d951c0a4b9669f6c4689
guilhermepires <gguilhermepires@gmail.com>
2022-04-03 11:14:34 -0300

    adicionado o get

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const url = 'http://localhost:5000/api/v1/poi/tabela'
+  const [lista, setLista] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then(res => {
+      console.log(res);
+    }).catch(err=>{
+      console.log(err);
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        
+      </body>
     </div>
   );
 }
