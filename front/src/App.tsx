bd0aa93d666b8b769297b4ed6ef731ae25f45c4d
guilhermepires <gguilhermepires@gmail.com>
2022-04-04 20:33:15 -0300

    refatoracao

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,60 +1,84 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
-import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
-import { json } from 'stream/consumers';
-import { textSpanEnd } from 'typescript';
+import InputMask from 'react-input-mask';
+import { Card, ListGroup, ListGroupItem, Row, Col, Button } from 'react-bootstrap';
 function App() {
   const url = 'http://localhost:5000/api/v1/poi/tabela'
   let [lista, setLista] = useState<any>([]);
+  let [placa, setPlaca] = useState<any>('');
+  let [data, setData] = useState<any>('');
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      console.log('This will run every second!');
-      axios.get(url).then(res => {
-        console.log('data');
-        console.log(res.data);
-        console.log('dado');
-        console.log(res.data.dado);
-        console.log('dado length');
-        console.log(res.data.dado.length);
-
-        let tmp = [];
-        for (let i = 0; i < res.data.dado.length; i++) {
-          tmp.push(res.data.dado[i]);
-        }
-        setLista(tmp);
-      }).catch(err => {
-        console.log(err);
-      });
-    }, 5000);
-    return () => clearInterval(interval);
+    buscaTabela();
   }, []);
 
+  const buscaTabela = () => {
+    axios.get(`${url}?data=${data}&placa=${placa}`).then(res => {
+      let tmp = [];
+      for (const property in res.data.dado) {
+        tmp.push(res.data.dado[property]);
+      }
+      setLista(tmp);
+    }).catch(err => {
+      console.log(err);
+    });
+  } 
+  
   const montaTabela = () => {
     var temp = lista.map((elem: any) => {
-      return (
-        <Card>
-          <Card.Header as="h5">Ponto: {elem.ponto.nome}</Card.Header>
-          <Card.Body>
-            <Card.Title>Raio: {elem.ponto.raio}</Card.Title>
-            <Card.Text>
-              <ListGroup>
-                {
-                  montalista(elem.tempoVeiculos)
-                }
-              </ListGroup>
-            </Card.Text>
-          </Card.Body>
-        </Card>
+      return (<div>
+        <br></br>
+        <Row >
+          <Col sm="3"></Col>
+          <Col sm="6">
+            <Card >
+              <Card.Header as="h5">Ponto: {elem.ponto.nome}</Card.Header>
+              <Card.Body>
+                <Card.Title>Raio: {elem.ponto.raio}</Card.Title>
+                <Card.Text>
+                  <ListGroup>
+                    {
+                      montalista(elem.tempoVeiculos)
+                    }
+                  </ListGroup>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
       );
     });
-    console.log(temp);
-
     return (temp);
   };
+
+  const onChangePlaca = (event: any) => {
+    setPlaca(event.target.value);
+  }
+
+  const onChangeData = (event: any) => {
+    setData(event.target.value);
+  }
+
+  const onClickBusca = () => {
+    buscaTabela();
+  }
+
   return (
     <div className="App">
+      <Row>
+        <Col sm="4"></Col>
+        <Col sm="3"><h1>Desafio técnico</h1></Col>
+      </Row>
+      <br></br>
+      <Row>
+        <Col sm="3"></Col>
+        <Col sm="6">Data: <InputMask mask="99/99/9999" value={data} onChange={onChangeData} />
+          {'  '} Placa: <input value={placa} onChange={onChangePlaca} />
+          {'  '} <Button variant="primary" onClick={onClickBusca}>Buscar</Button>
+        </Col>
+      </Row>
       {
         montaTabela()
       }
@@ -68,31 +92,25 @@ export default App;
 function montalista(tempoVeiculos: any) {
   let lista = [];
   for (let key in tempoVeiculos) {
-    console.log(tempoVeiculos[key]);
     lista.push((
       <div>
-        <ListGroupItem
-        active
-        >
+        <ListGroupItem active >
           {tempoVeiculos[key].placa}
         </ListGroupItem>
         <ListGroupItem>
           Tempo em Dias: {tempoVeiculos[key].tempo.dia}
         </ListGroupItem>
         <ListGroupItem>
-        Tempo em horas: {tempoVeiculos[key].tempo.hora}
+          Tempo em horas: {tempoVeiculos[key].tempo.hora}
         </ListGroupItem>
         <ListGroupItem>
-        Tempo em minutos: {tempoVeiculos[key].tempo.minuto}
+          Tempo em minutos: {tempoVeiculos[key].tempo.minuto}
         </ListGroupItem>
         <ListGroupItem>
-        Tempo em segundos: {tempoVeiculos[key].tempo.segundo}
+          Tempo em segundos: {tempoVeiculos[key].tempo.segundo}
         </ListGroupItem>
       </div>
-
-
     ));
-
   }
   return (lista);
 }
